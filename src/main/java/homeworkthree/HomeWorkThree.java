b8eb5383da233d03156d59f7252c738d681bcc0e
Natalya <natsnegireva@gmail.com>
2020-07-05 16:50:02 +0300

    fintshed homeworkthree

--- src/main/java/homeworkthree/HomeWorkThree.java
+++ src/main/java/homeworkthree/HomeWorkThree.java
@@ -9,9 +9,10 @@ public class HomeWorkThree {
 
     public static void main(String[] args) {
         ScanerTest(9);
+        scanner.close();
     }
 
-    // Задание 1: Написать метод, которая загадывает случайное число от 0 до 9
+    // Задание 1: Написать метод, который загадывает случайное число от 0 до 9
     // пользователю дается 3 попытки угадать это число.
     // При каждой попытке сообщить, больше ли указанное пользователем число, чем загаданное, или меньше.
     // После победы или проигрыша выводится запрос – «Повторить игру еще раз?
@@ -20,8 +21,8 @@ public class HomeWorkThree {
     public static void ScanerTest(int range) {
         System.out.println("Введите число от 0 до " + range + ", у вас есть 3 попытки");
         int randomNumber = (int) (Math.random() * range); // сгенерирует случайное не отрицательное число до range
-        for (int count = 0; count < 3; countint input_number = scanner.nextInt();++) {
-
+        for (int count = 0; count < 3; count++) {
+            int input_number = scanner.nextInt();
             if (input_number == randomNumber) {
                 System.out.println("Вы угадали!");
                 System.out.println("Попробовать еще? Введите 1, если ДА или 0, если НЕТ");
@@ -46,7 +47,7 @@ public class HomeWorkThree {
                 }
             }
         }
-        scanner.close();
+
     }
 }
 
