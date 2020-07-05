cc7a9ff09542964a0522646ca4b23ddd3649b9a7
Natalya <natsnegireva@gmail.com>
2020-07-05 16:54:08 +0300

    try to use

--- src/test/java/homeworkthree/HomeWorkThreeTest.java
+++ src/test/java/homeworkthree/HomeWorkThreeTest.java
@@ -1,42 +1,21 @@
-package homeworkthree;
+//package homeworkthree;
+//
+//import homeworkthree.HomeWorkThree; // делаем импорт класса
+//import org.junit.Test; // добавляем библиотеку
+//import static org.junit.Assert.assertFalse;
 
-import homeworkthree.HomeWorkThree; // делаем импорт класса
-import org.junit.Test; // добавляем библиотеку
-import static org.junit.Assert.assertFalse;
+//public class HomeWorkThreeTest { // в основном классе файла прописываем все
 
-public class HomeWorkThreeTest { // в основном классе файла прописываем все
+   // public HomeWorkThree HomeWorkThree = new HomeWorkThree(); // создаем копию класса добавляем обьект Test
 
-    public HomeWorkThree HomeWorkThree = new HomeWorkThree(); // создаем копию класса добавляем обьект Test
-
-    //@Test
-    /*static void playLevel(int range, int number) {
-        while (true) {
-            int input_number = scanner.nextInt();
-            if (input_number == number) {
-                System.out.println("Вы угадали!");
-                System.out.println("«Повторить игру еще раз? введите 1 – да или 0 – нет»");
-                int input_number1 = scanner.nextInt();
-                if (input_number1 == 1) {
-                    break;
-                } else if (input_number1 == 0) {
-                    scanner.close();
-                }
-                break;
-            } else if (input_number > number) {
-                System.out.println("Ваше число больше, чем загаданное");
-            } else if (input_number < number) {
-                System.out.println("Ваше число меньше, чем загаданное");
-            } else {
-                System.out.println("Вы не угадали");
-                System.out.println("«Повторить игру еще раз? введите 1 – да или 0 – нет»");
-                break;
-            }
-            assertFalse(boolean);
-            Object assertArray;
-            assertArray.equals(trueArr, falseArr);
-        }
-    }*/
+//    @Test
+//    public static void ScanerTest(int range) {
+//            assertFalse(boolean);
+//            Object assertArray;
+//            assertArray.equals(trueArr, falseArr);
+//        }
+//    }
 
 
 
-}
+//}
