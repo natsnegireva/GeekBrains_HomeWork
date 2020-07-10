503429244ec862077a1f33bc50c98e89063b0155
Natalya <natsnegireva@gmail.com>
2020-07-10 15:46:45 +0300

    fixed bugs in homework four

--- src/main/java/homeworkfour/HomeWorkFour.java
+++ src/main/java/homeworkfour/HomeWorkFour.java
@@ -12,10 +12,6 @@ public class HomeWorkFour {
     public final static char DOT_X = 'X'; // заданная константа
     public final static char DOT_O = 'O'; // заданная константа
     public final static char EMPTY_DOT = '•'; // заданная константа
-    public static int x;
-    public static int y;
-    public static char symb;
-
 
     public static void main(String[] args) {
         initMap(); // создаем карту
@@ -59,25 +55,53 @@ public class HomeWorkFour {
     private static boolean isTurnValid(int y, int x) {
         if (y < 0 || y > SIZE - 1 || x < 0 || x > SIZE - 1) { // если вышли за область поля
             return false;
-        }
-        if (map[y][x] == EMPTY_DOT) { // если поле свободно для игры
+        } else if (map[y][x] == EMPTY_DOT) { // если поле свободно для игры
             return true;
         }
-
-        return false;
     }
 
     private static void aITurn() { // ход компьютера
+        int x, y;
+        do {
+            x = (int)(Math.random() * SIZE);
+            y = (int)(Math.random() * SIZE);
+        } while (!isTurnValid(y, x));
+        map[y][x] = DOT_O;
+    }
+
+    // Проба переписать под блокировку выигрыша игрока
+    private static void aITurn2 () { // ход компьютера с блокировкой игрока
+        int x, y;
         System.out.println("AI turned");
         do {
-            map[y][x] = DOT_O;
-            y = (int) (Math.random() * SIZE);
-            x = (int) (Math.random() * SIZE);
-        } while (!isTurnValid(x, y));
-
+            if (map[0][0] == map[0][1] && map[2][2] == map[1][2] && map[2][0] == map[1][1]) {
+                y = 0; x = 2;
+            } else if (map[2][0] == map[2][1] && map[1][2] == map[0][2] && map[0][0] == map[1][1]) {
+                y = 2; x = 2;
+            } else if (map[0][1] == map[0][2] && map[1][0] == map[2][0] && map[1][1] == map[2][2]) {
+                y = 0; x = 0;
+            } else if (map[1][0] == map[0][0] && map[2][1] == map[2][2] && map[1][1] == map[0][2]) {
+                y = 2; x = 0;
+            } else if (map[0][0] == map[2][2] && map[0][2] == map[2][0] && map[0][1] == map[2][1] && map[1][0] == map[1][2]) {
+                y = 1; x = 1;
+            } else if (map[1][1] == map[1][2] && map[0][0] == map[2][0]) {
+                y = 1; x = 0;
+            } else if (map[1][1] == map[2][1] && map[0][0] == map[0][2]) {
+                y = 0; x = 1;
+            } else if (map[0][2] == map[2][2] && map[1][0] == map[1][1]) {
+                y = 1; x = 2;
+            } else if (map[1][1] == map[0][1] && map[2][0] == map[2][2]) {
+                y = 2; x = 1;
+            } else {
+                y = sc.nextInt(SIZE);
+                x = sc.nextInt(SIZE);
+            }
+        } while (!isTurnValid(y, x));
+        map[y][x] = DOT_O;
     }
 
     private static void humanTurn() { // ход игрока
+        int x, y;
         System.out.println("Please input dot coordinates, in format Y X");
         do {
             y = sc.nextInt();
@@ -114,11 +138,11 @@ public class HomeWorkFour {
     // с использованием циклов.
     private static boolean isWin(char symbol) {
         for (int i = 0; i < SIZE; i++) {
-            if ((map[i][0] == symbol && map[i][1] == symbol && map[i][2] == symbol) ||
-                    (map[0][i] == symbol && map[1][i] == symbol && map[2][i] == symbol) ||
-                    (map[0][0] == symbol && map[1][1] == symbol && map[2][2] == symbol) ||
-                    (map[2][0] == symbol && map[1][1] == symbol && map[0][2] == symbol)) {
-                return true;
+            for (int j = 0; j < SIZE; j++) {
+                if (map[i][0] == symbol && map[i][1] == symbol && map[i][2] == symbol) return true;
+                if (map[0][j] == symbol && map[1][j] == symbol && map[2][j] == symbol) return true;
+                if (map[0][0] == symbol && map[1][1] == symbol && map[2][2] == symbol) return true;
+                if (map[2][0] == symbol && map[1][1] == symbol && map[0][2] == symbol) return true;
             }
         }
         return false;
@@ -128,25 +152,26 @@ public class HomeWorkFour {
     // Очень желательно не делать это просто набором условий для каждой из возможных ситуаций;
     private static boolean isWin(char symbol) {
         for (int i = 0; i < SIZE; i++) {
-            if ((map[i][0] == symbol && map[i][1] == symbol && map[i][2] == symbol && map[i][3] == symbol && map[i][4] == symbol) ||
-                    (map[0][i] == symbol && map[1][i] == symbol && map[2][i] == symbol && map[3][i] == symbol && map[4][i] == symbol) ||
-                    (map[0][0] == symbol && map[1][1] == symbol && map[2][2] == symbol && map[3][3] == symbol && map[4][4] == symbol) ||
-                    (map[4][0] == symbol && map[3][1] == symbol && map[2][2] == symbol && map[1][3] == symbol && map[0][4] == symbol)) {
-                return true;
+            for (int j = 0; j < SIZE; j++) {
+            if (map[i][0] == symbol && map[i][1] == symbol && map[i][2] == symbol && map[i][3] == symbol && map[i][4] == symbol) return true;
+            if (map[0][j] == symbol && map[1][j] == symbol && map[2][j] == symbol && map[3][j] == symbol && map[4][j] == symbol) return true;
+            if (map[0][0] == symbol && map[1][1] == symbol && map[2][2] == symbol && map[3][3] == symbol && map[4][4] == symbol) return true;
+            if (map[4][0] == symbol && map[3][1] == symbol && map[2][2] == symbol && map[1][3] == symbol && map[0][4] == symbol) return true;
+               }
             }
-        }
-        return false;
+         return false;
     }
+
     // вариант с урока
     public static boolean isWin(char symbol) {
-        if(map[0][0] == symbol && map[0][1] == symbol && map[0][2] == symbol) return true;
-        if(map[1][0] == symbol && map[1][1] == symbol && map[1][2] == symbol) return true;
-        if(map[2][0] == symbol && map[2][1] == symbol && map[2][2] == symbol) return true;
-        if(map[0][0] == symbol && map[1][0] == symbol && map[2][0] == symbol) return true;
-        if(map[0][1] == symbol && map[1][1] == symbol && map[2][1] == symbol) return true;
-        if(map[0][2] == symbol && map[1][2] == symbol && map[2][2] == symbol) return true;
-        if(map[0][0] == symbol && map[1][1] == symbol && map[2][2] == symbol) return true;
-        if(map[2][0] == symbol && map[1][1] == symbol && map[0][2] == symbol) return true;
+        if (map[0][0] == symbol && map[0][1] == symbol && map[0][2] == symbol) return true;
+        if (map[1][0] == symbol && map[1][1] == symbol && map[1][2] == symbol) return true;
+        if (map[2][0] == symbol && map[2][1] == symbol && map[2][2] == symbol) return true;
+        if (map[0][0] == symbol && map[1][0] == symbol && map[2][0] == symbol) return true;
+        if (map[0][1] == symbol && map[1][1] == symbol && map[2][1] == symbol) return true;
+        if (map[0][2] == symbol && map[1][2] == symbol && map[2][2] == symbol) return true;
+        if (map[0][0] == symbol && map[1][1] == symbol && map[2][2] == symbol) return true;
+        if (map[2][0] == symbol && map[1][1] == symbol && map[0][2] == symbol) return true;
         return false;
     }
 
