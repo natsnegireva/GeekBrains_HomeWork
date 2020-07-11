27acf5edc438b7d51bea363414f6cd83041b033b
Natalya <natsnegireva@gmail.com>
2020-07-11 19:55:07 +0300

    Main homeworkfive

---
+++ src/main/java/homeworkfive/Main.java
@@ -0,0 +1,14 @@
+package homeworkfive;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Staff personels = new Staff();
+        System.out.println(personels.fullName);
+        personels.age = 15;
+        personels.fullName = "default";
+
+    }
+
+
+}
