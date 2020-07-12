5535be38c85265852bc9d9e940def0eb934bd22f
Natalya <natsnegireva@gmail.com>
2020-07-12 15:52:50 +0300

    finish homeworkfive

--- src/main/java/homeworkfive/Main.java
+++ src/main/java/homeworkfive/Main.java
@@ -3,12 +3,13 @@ package homeworkfive;
 public class Main {
 
     public static void main(String[] args) {
-        Staff personels = new Staff();
-        System.out.println(personels.fullName);
-        personels.age = 15;
-        personels.fullName = "default";
+
+        // создание нового обьекта в классе Main
+        Staff personel = new Staff("Селезнев Тимур Максимович",
+                "грузчик 4 разряда", "grip@mail.ru",
+                "+79109765385", 30000, 35);
+        System.out.println(personel); // печатает ссылку на память
 
     }
-
-
 }
+
