bd19efa9a8b3ca0fd54b468eba360fbea1541588
Natalya <natsnegireva@gmail.com>
2020-09-05 21:08:51 +0300

    finished homeworkthree 4

--- src/main/java/homeworkthree3/clientChat/ClientApp.java
+++ src/main/java/homeworkthree3/clientChat/ClientApp.java
@@ -1,25 +1,35 @@
 package homeworkthree3.clientChat;
 
 import javafx.application.Application;
+import javafx.application.Platform;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Parent;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 public class ClientApp extends Application {
+    private static Stage stage;
 
 
     public static void main(String[] args) {
         launch(args);
-        new ClientService().run();
     }
 
     @Override
     public void start(Stage primaryStage) throws Exception {
-        FXMLLoader fxmlLoader = new FXMLLoader(getClass().getResource("/Client.fxml"));
-        Parent root = fxmlLoader.load();
-        primaryStage.setTitle("My Chat");
-        primaryStage.setScene(new Scene(root, 600, 500));
+        stage = primaryStage;
+
+        Parent root = FXMLLoader.load(getClass().getResource("/Client.fxml"));
+        Image image = new Image(getClass().getResourceAsStream("/chat.png"));
+        primaryStage.setTitle("Чат для девчат");
+        primaryStage.getIcons().add(image);
+        primaryStage.setResizable(false);
+        primaryStage.setScene(new Scene(root, 500, 400));
+        primaryStage.setOnCloseRequest(e -> Platform.exit());
         primaryStage.show();
+    }
+    public static Stage getStage() {
+        return stage;
     }
 }
--- src/main/java/homeworkthree3/clientChat/ClientService.java
+++ src/main/java/homeworkthree3/clientChat/ClientService.java
@@ -1,15 +1,16 @@
 package homeworkthree3.clientChat;
 
-import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.TextArea;
 import javafx.scene.control.TextField;
 
-import java.io.DataInputStream;
-import java.io.DataOutputStream;
-import java.io.IOException;
+import java.io.*;
 import java.net.Socket;
+import java.time.LocalDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ClientService implements Runnable {
 
@@ -25,50 +26,53 @@ public class ClientService implements Runnable {
     Button loginButton; // кнопка авторизации
     @FXML
     Button sendButton; // кнопка отправки сообщение
+    @FXML
+    TextField listArea; // поле для списка пользователей
+    @FXML
+    TextField nickField; // ПОЛЕ ДЛЯ ЛОГИНА
 
 
     private Socket socket;
     private static DataInputStream dis; // читаем
     private static DataOutputStream dos; // отсылаем
-    private String nick;
+    private String Mynick;
 
     @Override
     public void run() {
-        nick = "";
+        Mynick = nickField.getText();
         try {
-            socket = new Socket( "localhost", 8189 );
+            socket = new Socket( "localhost", 8173 );
             dis = new DataInputStream( socket.getInputStream() );
             dos = new DataOutputStream( socket.getOutputStream() );
             setAutorized( false );
             Thread t1 = new Thread( () -> {
                 try {
-                    while (true) {
-                        // После авторизации от сервера приходит сообщение формата «/authOk nick»,
-                        // клиент узнает, под каким ником авторизовался.
-                        String strMsg = dis.readUTF();
+                    while (socket.isConnected()) {
+                        String strMsg = null;
+                        strMsg = dis.readUTF();
                         if (strMsg.startsWith( "/authOk" )) {
                             setAutorized( true );
-                            nick = strMsg.split( "\\s" )[1]; // берем ник
+                            Mynick = strMsg.split( "\\s" )[1];
+                            listArea.appendText( Mynick + "\n" );
+                            getChat();
                             break;
+                        } else {
+                            addMessage( strMsg );
                         }
-                        chatArea.appendText( strMsg + "\n" );
-                    }
-                    while (true) {
-                        String strMsg = dis.readUTF(); // читаем
                         if (strMsg.equals( "/exit" )) {
+                            saveChat();
+                            addMessage( strMsg );
                             break;
                         }
-                        chatArea.appendText( strMsg + "\n" );
                     }
-                } catch (RuntimeException, IOException e) {
-                    e.printStackTrace();
+                } catch (IOException e) {
                     chatArea.appendText( "ОШИБКА СОЕДИНЕНИЯ" + "\n" );
                 } finally {
                     try {
                         setAutorized( false );
                         chatArea.appendText( "ЧАТ ЗАКРЫТ" + "\n" );
                         socket.close();
-                        nick = "";
+                        Mynick = "";
                     } catch (IOException e) {
                         e.printStackTrace();
                     }
@@ -77,58 +81,103 @@ public class ClientService implements Runnable {
             t1.setDaemon( true );
             t1.start();
         } catch (IOException e) {
-            chatArea.appendText( "Не удалось подключиться к серверу" );
             e.printStackTrace();
         }
     }
 
     // действия кнопки отправки сообщения с датой
-    public void sendMsgAction(ActionEvent actionEvent) {
-        if(setAutorized( true )) {
-            try {
-                dos.writeUTF( msgField.getText() + "\n");
-                chatArea.appendText(dis.readUTF());
-                msgField.clear();
-                msgField.requestFocus();
-            } catch (IOException e) {
-                chatArea.appendText( "СООБЩЕНИЕ НЕ УХОДИТ" + "\n");
-            }
+    public void sendMsgAction() {
+        if (setAutorized( true )) {
+            sendMessage( msgField.getText() );
+            msgField.clear();
         } else {
-            chatArea.appendText( "ВАМ НЕОБХОДИМО АВТОРИЗОВАТЬСЯ" );
-            sendButton.setVisible( false );
-            msgField.setVisible( false );
-            loginField.requestFocus();
+            chatArea.appendText( "ВАМ НЕОБХОДИМО АВТОРИЗОВАТЬСЯ" + "\n" );
+        }
+    }
+
+    // добавляем текст в поле чата
+    public void addMessage(String msg) {
+        DateTimeFormatter dtf = DateTimeFormatter.ofPattern( "HH:mm:ss" );
+        LocalDateTime now = LocalDateTime.now();
+        chatArea.appendText( dtf.format( now ) + " : " + msg + "\n" );
+        chatArea.selectPositionCaret( this.chatArea.getLength() );
+    }
+
+    // отсылает сообщение на сервер
+    public static void sendMessage(String msg) throws NullPointerException {
+        if (!msg.isEmpty()) {
+            try {
+                dos.writeUTF( msg + "\n" );
+                dos.flush();
+            } catch (IOException | NullPointerException e) {
+                e.printStackTrace();
+            }
         }
     }
 
     // действия кнопки пароля-логина
-    public void onAuthClickButton(ActionEvent actionEvent) throws NullPointerException {
-        if (loginField.getText() != null && passField.getText() != null) {
+    public void onAuthClickButton() throws NullPointerException {
+        if (loginField.getText() != null && passField.getText() != null && nickField.getText() != null) {
             try {
-                String logPass = loginField.getText() + "\\s" + passField.getText();
-                dos.writeUTF( "/auth" + logPass + "\n" );
-                passField.setVisible( false );
-                loginField.setVisible( false );
-                loginButton.setVisible( false );
-                msgField.requestFocus();
-            } catch (NullPointerException | IOException c) {
-                chatArea.appendText( "НЕ ВЕРНО ВВЕДЕН ЛОГИН ПАРОЛЬ" );
+                String msg = "/auth " + loginField.getText() + passField.getText() + nickField.getText();
+                sendMessage(msg);
+            } catch (NullPointerException e) {
+                e.printStackTrace();
+                chatArea.appendText( "НЕ ВЕРНО ВВЕДЕН ЛОГИН ПАРОЛЬ" + "\n" );
                 loginField.clear();
                 passField.clear();
-                loginField.requestFocus();
             }
         } else {
             setAutorized( false );
             loginField.clear();
             passField.clear();
-            sendButton.setVisible( false );
-            msgField.setVisible( false );
+            String msg = "";
         }
-        setAutorized(true);
     }
 
-    private boolean setAutorized(boolean b) {
+    public boolean setAutorized(boolean b) {
         return b;
+    }
+
+
+    // сохраняем историю поля чата
+    private void saveChat() throws IOException {
+        try {
+            File history = new File( "/Chat.txt" );
+            if (!history.exists()) {
+                history.createNewFile();
+            }
+            PrintWriter fileWriter = new PrintWriter( new FileWriter( history, false ) );
+            BufferedWriter bufferedWriter = new BufferedWriter( fileWriter );
+            bufferedWriter.write( chatArea.getText() );
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    // загружаем историю в поле чата
+    private void getChat() throws IOException {
+        int lengthStory = 100;
+        File history = new File( "/Chat.txt" );
+        List<String> historyList = new ArrayList<>();
+        FileInputStream in = new FileInputStream( history );
+        BufferedReader bufferedReader = new BufferedReader( new InputStreamReader( in ) );
+
+        String temp;
+        while ((temp = bufferedReader.readLine()) != null) {
+            historyList.add( temp );
+        }
+
+        if (historyList.size() > lengthStory) {
+            for (int i = historyList.size() - lengthStory; i <= (historyList.size() - 1); i++) {
+                chatArea.appendText( historyList.get( i ) + "\n" );
+            }
+        } else {
+            for (int i = 0; i < lengthStory; i++) {
+                System.out.println( historyList.get( i ) );
+            }
+        }
     }
 
 }
